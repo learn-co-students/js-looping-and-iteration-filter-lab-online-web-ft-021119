e5904ae197202d24f21901dea543703850f2da37
joanrig <github email address>
2019-05-24 16:41:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+function findMatching(drivers,string) {
+  const collection = []
+  for (const driver of drivers) {
+    if (driver.toLowerCase() === string.toLowerCase()) {
+      collection.push(console.log(driver));
+    }
+  }
+  return collection
+}
