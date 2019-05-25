15c8415ea8b1974113db8b6f6c5fe001d1bdef19
burakkocak884 <45079524+burakkocak884@users.noreply.github.com>
2019-05-25 11:47:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file
+ function findMatching(driver, name){
+ const newDriversArray = [];
+ 
+ for(const driverName of driver){
+
+ 	if (driverName.toLowerCase()  === name.toLowerCase()) { 		
+ 		 newDriversArray.push(driverName); 	
+ }
+}
+return newDriversArray;
+}
+function fuzzyMatch(drivers, firstTwoLetters){
+return drivers.filter(function(el) {
+      return el.charAt(0).toLowerCase() === firstTwoLetters.charAt(0).toLowerCase();
+  })
+}
+ function matchName(driver, name){
+const newDriversArray = [];
+
+ for(const firstName of driver){
+
+ 	if (firstName.name === name) {
+ 		
+ 		 newDriversArray.push(firstName);
+ }
+ }
+return newDriversArray;
+
+ }
