16b0f4741b362a9d8335a90e3e65bd7500b1bec8
bbennett7 <bryn.bennett@gmail.com>
2019-05-26 11:53:02 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(array, string) {
+  let matches = (array.filter(function(name) {return name === string || name === string.toLowerCase()}));
+
+  return matches;
+}
+
+function fuzzyMatch(array, string) {
+  let fuzzyMatches = (array.filter(function(name) {return name.startsWith(string)}));
+
+  return fuzzyMatches;
+}
+
+function matchName(array, string) {
+  let nameMatches = (array.filter(function(driver) {return driver.name === string}));
+
+  return nameMatches;
+}
